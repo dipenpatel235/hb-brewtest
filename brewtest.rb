0bf90d318131a4ccfd4244f9105817f4b7522189
Dipen <dipen.ec2010@gmail.com>
2018-03-20 19:12:56 +0800

    script

--- brewtest.rb
+++ brewtest.rb
@@ -1,19 +1,13 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/homebrew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Brewtest < Formula
   desc ""
   homepage ""
-  url "https://github.com/dipenpatel235/brewtest/archive/v1.0.0.tar.gz"
-  version "1.0.0"
-  sha256 "ff0c1b62c3cac2065f05817577305bbde606386316dcd45096c61e9e4cbc3d41"
+  url "https://github.com/dipenpatel235/brewtest/archive/v1.0.2.tar.gz"
+  version "1.0.2"
+  sha256 "7929b5a62d7da7a0bd3166d588d8b0a317dcbdff3086442d9387579dcc9a8d82"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-	bin.install brewtest
+ 	bin.install "brewtest"
   end
 
 end
